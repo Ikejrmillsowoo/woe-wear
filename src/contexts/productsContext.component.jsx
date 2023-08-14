8cc645207dc3bb8a1d6bb2d5f275a6cee7f6566c
poppymills2@gmail.com <poppymills2@gmail.com>
2023-08-13 21:20:44 -0400

    added in our addToCart feature

--- src/contexts/productsContext.component.jsx
+++ src/contexts/productsContext.component.jsx
@@ -2,12 +2,6 @@ import { createContext, useState, useEffect } from "react";
 
 import PRODUCTS from "../shop-data.json";
 
-// import {
-//   createUserDocumentFromAuth,
-//   onAuthStateChangedListener,
-//   signOutUser,
-// } from "../utils/firebase/firebase.utils";
-
 // the actual value
 export const ProductsContext = createContext({
   products: [],
@@ -18,19 +12,6 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState(PRODUCTS);
   const value = { products };
-
-  // signOutUser();
-
-  //   useEffect(() => {
-  //     const unsubscribe = onAuthStateChangedListener((product) => {
-  //       if (product) {
-  //         createUserDocumentFromAuth(product);
-  //       }
-  //       setCurrentUser(product);
-  //     });
-
-  //     return unsubscribe;
-  //   }, []);
 
   return (
     <ProductsContext.Provider value={value}>
